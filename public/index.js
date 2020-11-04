9a41e644fa91bbf33c8e195e10e283c088ba807b
Rishi Menon <rishimenon212@gmail.com>
2020-11-04 01:30:34 -0500

    Started creating the logo in the main menu

--- public/index.js
+++ public/index.js
@@ -2,39 +2,18 @@
 // const strDomain = 'https://rishi.loca.lt';
 const strDomain = window.location.toString();
 
-console.log ("Domain: " + strDomain)
+console.log("Domain: " + strDomain);
 
-const socket = io(strDomain)
+const socket = io(strDomain);
 
-socket.on ('connect', () => {
-    console.log ('connected')
-})
+socket.on("connect", () => {
+  console.log("connected");
+});
 
+window.onload = function () {
+  console.log("Hi there");
+};
 
-const canvas = document.getElementById ("mainMenuCanvas");
-const ctx = canvas.getContext ("2d");
-const width = canvas.width;
-const height = canvas.height;
-
-const fps = 40;
-const deltaTime = 1.0/fps;
-
-let g_mousePos = { x:0.0, y:0.0}; 
-
-window.onload = function() {
-    console.log ('Hi')
-
-    canvas.addEventListener ("mousemove", function(evt) {
-        let rect = canvas.getBoundingClientRect();
-        let root = document.documentElement;
-
-        g_mousePos.x = evt.clientX - rect.left- root.scrollLeft;
-        g_mousePos.x = evt.clientY - rect.top - root.scrollTop;
-
-        console.log (g_mousePos)
-    });
-
-    document.addEventListener ("keydown", function(evt) {
-
-    });
-}
+////////////////////////////////////////////////////////////////////////////////////
+///////////////                  Canvas
+////////////////////////////////////////////////////////////////////////////////////
