942a529e76f3f8ba3a0c58cef3f0e020d6625c7f
Rishi Menon <rishimenon212@gmail.com>
2020-12-27 00:36:59 -0500

    C1

--- sTemp.js
+++ sTemp.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const nPortNumber = 80;
+const nPortNumber = 3000;
 
-const server = app.listen(nPortNumber, () => console.log(`Listening at port: ${nPortNumber}`))
+// const server = app.listen(nPortNumber, () => console.log(`Listening at port: ${nPortNumber}`))
+const server = app.listen(nPortNumber)
 
 app.use ('/', express.static('public'))
