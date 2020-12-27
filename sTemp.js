d3d38b654964de728345b23de6d0eb0a1d0ba071
Rishi Menon <rishimenon212@gmail.com>
2020-12-27 00:43:32 -0500

    C2

--- sTemp.js
+++ sTemp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const nPortNumber = 3000;
+const nPortNumber = process.env.PORT || 3000;
 
 // const server = app.listen(nPortNumber, () => console.log(`Listening at port: ${nPortNumber}`))
 const server = app.listen(nPortNumber)
